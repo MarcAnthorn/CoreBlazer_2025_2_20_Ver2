5071e6330fed0bbe08af5e92ce35545b3cb6bc14
MarcAnthorn <137853745+MarcAnthorn@users.noreply.github.com>
2025-02-20 16:42:33 +0800

    模块上传
    添加了事件中心模块、单例基类模块、UI管理器模块

---
+++ Assets/Scripts/Managers/EventHub.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+//这是一个基于观察者模式+单例模式实现的事件中心模块；
+//可以通过发布+订阅的方式，进行脚本之间的方法调用
+
+
+//声明一个抽象类
+public abstract class EventInfoBaseClass { }
+
+
+//事件中心的数据容器单元（含参数）
+//注意：这两步要看懂，为什么不直接声明一个泛型类作为下方字典的value类型，而要声明一个抽象类作为字典的value类型，然后去用里氏替换原则装载这个泛型类？
+//本质上是为了保证事件中心模块的唯一性，即全局只有一类事件中心模块；
+public class EventInfo<T> : EventInfoBaseClass
+{
+    public UnityAction<T> action_;
+
+    //构造函数，便于外部实例化时顺带传入监听方法；
+    public EventInfo(UnityAction<T> actions)
+    {
+        action_ += actions;
+    }
+}
+
+
+
+//事件中心的数据容器单元（不含参数）
+public class EventInfoNoPara : EventInfoBaseClass
+{
+    public UnityAction action_;
+
+    public EventInfoNoPara(UnityAction action)
+    {
+        action_ += action;
+    }
+}
+
+
+//事件中心模块：继承自单例模式基类
+
+public class EventHub : SingletonBaseManager<EventHub>
+{
+    private EventHub() { }
+
+    internal void EventTrigger<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    //如果事件中心模块这样定义，同时字典这样写：
+    //public class EventHub<T> : SingletonBaseManager<EventHub<T>>
+    //private Dictionary<string, EventInfo<T>> eventDictionary = new Dictionary<string, EventInfo<T>>();
+    //实际上就是违背了全局只有一种事件中心模块的规则；
+    //因为对于每个传入的T类型的数据，实际上都是一个全新类型的事件中心模块；
+    //因此，我们选择使用一个非泛型的抽象类作为字典的value，同时使用里氏替换原则，利用抽象类对象来装载其泛型子类的实例；
+    //这样就不会出现因为泛型 而被迫违背事件中心的唯一性了；
+
+    private Dictionary<string, EventInfoBaseClass> eventDictionary = new Dictionary<string, EventInfoBaseClass>();
+
+    public void EventTrigger<T>(string eventName, T info)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+        {
+            (eventDictionary[eventName] as EventInfo<T>).action_?.Invoke(info);      //info被作为参数传入；此时所有订阅者的监听方法都会收到该参数；
+        }
+    }
+
+
+    //支持多播委托的事件订阅；
+    public void AddEventListener<T>(string eventName, UnityAction<T> function)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+            (eventDictionary[eventName] as EventInfo<T>).action_ += function;  //as父转子实现子类成员委托调用
+        else
+        {
+            eventDictionary.Add(eventName, new EventInfo<T>(function));     //里氏替换父装载子类实例
+        }
+    }
+
+    //重载：不支持多播委托的事件订阅；
+    public void AddEventListenerNotMultuple<T>(string eventName, UnityAction<T> function)
+    {
+        if (eventDictionary.ContainsKey(eventName) && (eventDictionary[eventName] as EventInfo<T>).action_ == null)
+            (eventDictionary[eventName] as EventInfo<T>).action_ += function;  //as父转子实现子类成员委托调用
+        else
+        {
+            eventDictionary.Add(eventName, new EventInfo<T>(function));     //里氏替换父装载子类实例
+        }
+    }
+
+
+
+
+    //从特定key下指定移除订阅的方法
+    public void RemoveEventListener<T>(string eventName, UnityAction<T> function)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+            (eventDictionary[eventName] as EventInfo<T>).action_ -= function;   //根据里氏替换原则，就算父类对象中装载了子类实例，要想访问子类中的成员，也必须要使用as进行转换
+    }
+
+
+    //清楚所有订阅的方法
+    public void ClearListener()
+    {
+        eventDictionary.Clear();
+    }
+
+
+    //移除特定key的所有订阅事件
+    public void ClearListener(string eventName)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+            eventDictionary.Remove(eventName);
+    }
+
+
+
+
+    //所有订阅事件和发布事件的无参的重载
+    public void EventTrigger(string eventName)  //默认为null，这样外部就算没有信息可交流， 不传参，也不会报错；
+    {
+        if (eventDictionary.ContainsKey(eventName))
+        {
+            (eventDictionary[eventName] as EventInfoNoPara).action_?.Invoke();      //info被作为参数传入；此时所有订阅者的监听方法都会收到该参数；
+        }
+    }
+
+    public void AddEventListener(string eventName, UnityAction function)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+            (eventDictionary[eventName] as EventInfoNoPara).action_ += function;  //as父转子实现子类成员委托调用
+        else
+        {
+            eventDictionary.Add(eventName, new EventInfoNoPara(function));     //里氏替换父装载子类实例
+        }
+    }
+
+    public void RemoveEventListener(string eventName, UnityAction function)
+    {
+        if (eventDictionary.ContainsKey(eventName))
+            (eventDictionary[eventName] as EventInfoNoPara).action_ -= function;   //根据里氏替换原则，就算父类对象中装载了子类实例，要想访问子类中的成员，也必须要使用as进行转换
+    }
+
+
+}
+
+
+
---
+++ Assets/Scripts/Managers/SingletonBaseManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Reflection;
+
+//单例模式的基类
+//声明为抽象类；成为抽象类之后，该类就不能在任何地方被实例化；
+//基类的唯一性得到了保证：
+
+
+public abstract class SingletonBaseManager<T> where T : class  //new()    //注意：由于子类声明了私有的构造函数，我们不得不删除new()这个限制；
+{
+    //使用反射解决：
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                //Type的使用，需要引用命名空间：System
+                Type type = typeof(T);
+                //BingingFlags需要引用命名空间：System.Reflections
+                //下面这一步就是实现获取T类内部的私有构造函数：
+                ConstructorInfo info = type.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic,
+                                                            null,
+                                                            Type.EmptyTypes,
+                                                            null);
+                if (info != null)
+                    instance = info.Invoke(null) as T;
+                //该语句会返回Object类型的实例化对象；使其转换类型后被引用，就实现了在基类内部的子类的实例化
+                else
+                    Debug.Log("未得到对应的子类无参构造函数");
+                //设置一个提示，防止忘记声明私有构造函数；
+
+            }
+            return instance;
+        }
+    }
+
+}
