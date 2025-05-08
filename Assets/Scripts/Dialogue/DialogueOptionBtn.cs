36d22f5f6a2059bbf5c3f2b0f1f76f21bd4d3648
MarcAnthorn <137853745+MarcAnthorn@users.noreply.github.com>
2025-05-08 23:43:27 +0800

    11

--- Assets/Scripts/Dialogue/DialogueOptionBtn.cs
+++ Assets/Scripts/Dialogue/DialogueOptionBtn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -21,7 +22,7 @@ public class DialogueOptionBtn : MonoBehaviour
     void Start()
     {
         //进行特殊判断：2111的选项：1003 & 1012，如果节点9没有触发，那么这两个选项不可选择：
-        if(!GameLevelManager.Instance.avgIndexIsTriggeredDic[1009] && myOrder.rootId == 2111 && (myOrder.orderId == 1003 || myOrder.orderId == 1012))
+        if(GameLevelManager.Instance.avgIndexIsTriggeredDic.ContainsKey(1009) && !GameLevelManager.Instance.avgIndexIsTriggeredDic[1009] && myOrder.rootId == 2111 && (myOrder.orderId == 1003 || myOrder.orderId == 1012))
         {
             mask.SetActive(true);
         }
@@ -33,6 +34,7 @@ public class DialogueOptionBtn : MonoBehaviour
             {
                 //如果是，那么清理当前的avgpanel的回调，替换成直接给物品：
                 
+
 
             }
 
